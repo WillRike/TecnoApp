5d67977dc5a05674fe486172c6443a0e5c2e2e32
WillRike <will_rike@hotmail.com>
2021-12-14 21:05:20 -0300

    config Ambiente de Desenvolvimento

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'expo',
+  rules: {
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+  },
+};
